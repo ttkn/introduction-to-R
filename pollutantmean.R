7bd761578613d9ef9311222bb15a7d0156684277
ttkn <tonytran55@gmail.com>
2015-08-25 15:43:24 -0700

    Update pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,23 +1,20 @@
-pollutantmean <- function(directory, pollutant, id = 1:332) {
-  ## 'directory' is a character vector of length 1 indicating
-  ## the location of the CSV files
-  xfiles <- list.files("C:\\Users\\Pearl\\Documents\\r\\specdata", full.names = TRUE) 
-  xfiles2 <- xfiles[id]
-  
-  garganta <- do.call( #a lucky instance of do.call working correctly b/c of rbind
-    "rbind",lapply(xfiles2, FUN=function(files)
-      {read.csv(files, header=TRUE, sep=",")}))
-  mean(garganta[, pollutant], na.rm = TRUE)
-  
+  ## Returns the mean of the pollutant across all monitors listed
+  ## in the 'id' vector (ignoring NA values)
   ## 'pollutant' is a character vector of length 1 indicating
   ## the name of the pollutant for which we will calculate the
   ## mean; either "sulfate" or "nitrate".
-  
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the CSV files
   ## 'id' is an integer vector indicating the monitor ID numbers
   ## to be used
   
-  ## Return the mean of the pollutant across all monitors list
-  ## in the 'id' vector (ignoring NA values)
-  ## NOTE: Do not round the result!
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+  xfiles <- list.files("C:\\Users\\Pearl\\Documents\\r\\specdata", full.names = TRUE) 
+  xfiles2 <- xfiles[id]
+  
+  garganta <- do.call(
+    "rbind",lapply(xfiles2, FUN=function(files)
+      {read.csv(files, header=TRUE, sep=",")}))
+  
+  mean(garganta[, pollutant], na.rm = TRUE)
 }
-
